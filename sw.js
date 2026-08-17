bf44756620853bd7d8480ac2921b77840d7db708
kevinlabens-del <kevin.labens@gmail.com>
2026-08-17 19:39:18 +0200

    V13.1 : force le nouveau catalogue par catégories

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='devis-electrique-v13-direct-2';
+const CACHE='devis-electrique-v13-1-categories';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./catalog-v13.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>{event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
